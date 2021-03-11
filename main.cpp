b9455568c6e64c39fcce9754ee9fb14f91cbe3c0
githackathon <80451747+githackathon@users.noreply.github.com>
2021-03-11 09:39:42 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class MyType {
+public:
+	typedef enum enType {
+		ZERO = 0,
+		ONE,
+		TWO,
+		FOUR_HUNDRED = 400,
+		FF_AND_42 = 0xFF42
+	} T_Type;
+
+	T_Type m_eType;
+	MyType(T_Type eSetType) : m_eType(eSetType) {};
+	T_Type getType() { return m_eType; };
+};
+
+int main()
+{	
+ int i = 0xFF43;
+ char c;
+ 
+ cout << "\nKlocwork_Test!\n" << endl;
+  
+ // Trying to detect this
+ c = i; // 0x43 ascii char -> 'C'
+ cout << "c = " << c << endl; // Expected output: c = C (ASCII 0x43 is 'C')
+ 
+ // And this
+ MyType obj(MyType::FF_AND_42);
+ c = obj.getType();
+ cout << "c = " << c << endl; // Expected output: c = B (ASCII 0x42 is 'B')
+ 
+ // Buffer overflow - Just to check Klocwork desktop analysis is working
+ char array[3];
+ array[3] = 'z';
+ 
+ return 0;
+}
